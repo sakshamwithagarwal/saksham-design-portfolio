c1bfbc41b23aa59b1d34a50016a51eaa96299734
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2024-01-07 15:03:23 +0530

    Spotify temp

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,11 +3,15 @@ import { getNowPlaying } from "../../lib/spotify";
 import AboutCL from "./AboutCL";
 
 const About = async () => {
-  const song = await getNowPlaying();
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
-  // console.log(song);
-  return <AboutCL songURL={songUrl} albumArt={albumImageUrl} />
+
+  // const song = await getNowPlaying();
+  // const albumImageUrl = song.item.album.images[0].url;
+  const albumImageUrl =
+    "https://i.scdn.co/image/ab67616d0000b2738a70ce320d5c991f47f52b6f";
+  const songUrl = "https://open.spotify.com/track/7rwZD6MHLSuF2d9h5bnZPz";
+  // const songUrl = song.item.external_urls.spotify;
+  console.log(albumImageUrl, songUrl);
+  return <AboutCL songURL={songUrl} albumArt={albumImageUrl} />;
 };
 
 export default About;
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,11 +3,14 @@ import ContactCL from "./Contact";
 import { getNowPlaying } from "../../lib/spotify";
 
 const Contact = async () => {
-  const song = await getNowPlaying();
-  const albumImageUrl = song.item.album.images[0].url;
-  const songUrl = song.item.external_urls.spotify;
+  // const song = await getNowPlaying();
+  // const albumImageUrl = song.item.album.images[0].url;
+  // const songUrl = song.item.external_urls.spotify;
+  const albumImageUrl =
+    "https://i.scdn.co/image/ab67616d0000b2738a70ce320d5c991f47f52b6f";
+  const songUrl = "https://open.spotify.com/track/7rwZD6MHLSuF2d9h5bnZPz";
   // console.log(albumImageUrl, songUrl);
-  return <ContactCL albumArt={albumImageUrl} songURL={songUrl} />;
+  return <ContactCL />;
 };
 
 export default Contact;
