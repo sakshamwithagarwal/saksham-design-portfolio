a9ecde2c65fe4773b3527b37f761ef34799526bf
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2024-01-11 08:40:19 +0530

    fix: Spotify API song fetch

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,12 +2,19 @@ import "./about.css";
 import { getNowPlaying } from "@/lib/spotify";
 import AboutCL from "./AboutCL";
 
+function isEmpty(value) {
+  for (let prop in value) {
+    if (value.hasOwnProperty(prop)) return false;
+  }
+  return true;
+}
+
 const About = async () => {
   const song = await getNowPlaying();
-  const albumImageUrl = song
+  const albumImageUrl = !isEmpty(song)
     ? song.item.album.images[0].url
     : "https://i.scdn.co/image/ab67616d0000b2738a70ce320d5c991f47f52b6f";
-  const songUrl = song
+  const songUrl = !isEmpty(song)
     ? song.item.external_urls.spotify
     : "https://open.spotify.com/track/7rwZD6MHLSuF2d9h5bnZPz";
 
