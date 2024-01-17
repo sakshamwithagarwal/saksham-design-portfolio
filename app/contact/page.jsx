adec66c56bd6653fcb382a82a22645398b873350
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2024-01-17 16:36:27 +0530

    t

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,13 +4,11 @@ import { getNowPlaying } from "@/lib/spotify";
 
 const Contact = async () => {
   const song = await getNowPlaying();
-  const isPlaying = song.is_playing;
-  const albumImageUrl = isPlaying
-    ? song.item.album.images[0].url
-    : "https://i.scdn.co/image/ab67616d0000b2734bd59e05ec76adcea635b3d4";
-  const songUrl = isPlaying
-    ? song.item.external_urls.spotify
-    : "https://open.spotify.com/album/7hMLyLcxF0LyjxI93RjYAg";
+  const albumImageUrl = song.item.album.images[0].url;
+  const songUrl = song.item.external_urls.spotify;
+  // const albumImageUrl =
+  //   "https://i.scdn.co/image/ab67616d0000b2738a70ce320d5c991f47f52b6f";
+  // const songUrl = "https://open.spotify.com/track/7rwZD6MHLSuF2d9h5bnZPz";
 
   return <ContactCL albumArt={albumImageUrl} songURL={songUrl} />;
 };
