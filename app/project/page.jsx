fdab1f5512e8f5244a852065e2809b5dccaed095
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2024-01-23 21:09:13 +0530

    Dynamic api forced

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -1,37 +1,15 @@
 import React from "react";
 import AllProjectsPage from "./AllProjects";
-import { request } from "graphql-request";
 
 const getProjects = async () => {
-  try {
-    const { projects } = await request(
-      "https://ap-south-1.cdn.hygraph.com/content/clha5gtcw11sx01taepog266q/master",
-      `
-        query Projects {
-          projects {
-            id
-            projectName
-            projectDescription
-            projectThumbnail {
-              url
-              height
-              width
-              fileName
-            }
-            tags
-            slug
-            projectContent {
-              html
-            }
-          }
-        }
-        `
-    );
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/project`, {cache: "no-store"}
+  );
 
-    return projects;
-  } catch (err) {
-    throw new Error("Failed to fetch all projects.");
+  if (!response.ok) {
+    throw new Error("Error while fetching more projects.");
   }
+  return response.json();
 };
 
 const Page = async () => {
