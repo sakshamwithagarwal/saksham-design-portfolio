ac05e4c9cd8f01ea1c67e63694ad93b540f35c43
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2024-01-24 00:32:43 +0530

    Metadata update

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,31 @@
 import "./globals.css";
-import { Background, Cursor, Footer, Navbar, Noise, ScrollTop } from "@/components";
+import {
+  Background,
+  Cursor,
+  Footer,
+  Navbar,
+  Noise,
+  ScrollTop,
+} from "@/components";
 import { open_sans } from "@/utils/fonts";
 import Provider from "./provider";
 
 export const metadata = {
   title: "Saksham Parag Agarwal",
-  description: "Design portfolio",
+  description:
+    "Saksham Parag Agarwal, a 21-year-old product designer, intricately weaves inspiration from music and comics into his designs, reflecting a commitment to eco-consciousness and the right to repair in the design world.",
+  keywords: ["Design", "Product Design", "Branding", "Packaging"],
+  metadataBase: new URL("https://sakshamparag.com"),
+  openGraph: {
+    title: "Saksham Parag Agarwal",
+    description: "Design portfolio",
+    images: "/og-image.png",
+    type: "website",
+  },
+  twitter: {
+    title: "Saksham Parag Agarwal",
+    description: "Design portfolio",
+  },
 };
 
 export default function RootLayout({ children }) {
