6df96280917c61e606f54237da53e1eb516cc9f5
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2024-01-23 20:58:56 +0530

    Switch from fetch to direct graphql call

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import AllProjectsPage from "./AllProjects";
+import { request } from "graphql-request";
 
 const getProjects = async () => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/project`, {cache: "no-store"}
-  );
+  try {
+    const { projects } = await request(
+      "https://ap-south-1.cdn.hygraph.com/content/clha5gtcw11sx01taepog266q/master",
+      `
+        query Projects {
+          projects {
+            id
+            projectName
+            projectDescription
+            projectThumbnail {
+              url
+              height
+              width
+              fileName
+            }
+            tags
+            slug
+            projectContent {
+              html
+            }
+          }
+        }
+        `
+    );
 
-  if (!response.ok) {
-    throw new Error("Error while fetching more projects.");
+    return projects;
+  } catch (err) {
+    throw new Error("Failed to fetch all projects.");
   }
-  return response.json();
 };
 
 const Page = async () => {
