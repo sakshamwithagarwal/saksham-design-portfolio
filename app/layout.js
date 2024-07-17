c0c5dd1f891ffb7d95effd6a3136b794f057c9c2
Anshika Gautam <41854014+AnshikaG0219@users.noreply.github.com>
2024-07-17 22:54:58 +0530

    Dynamic Age and Figma iframe

--- app/layout.js
+++ app/layout.js
@@ -8,21 +8,22 @@ import {
 } from "@/components";
 import { open_sans } from "@/utils/fonts";
 import Provider from "./provider";
+import { getAge } from "@/lib/getAge";
 
 export const metadata = {
   title: "Saksham Parag Agarwal",
-  description:
-    "Saksham Parag Agarwal, a 21-year-old product designer, intricately weaves inspiration from music and comics into his designs, reflecting a commitment to eco-consciousness and the right to repair in the design world.",
+  description: `Saksham Parag Agarwal, a ${getAge()}-year-old product designer, intricately weaves inspiration from music and comics into his designs, reflecting a commitment to eco-consciousness and the right to repair in the design world.`
+    ,
   keywords: ["Design", "Product Design", "Branding", "Packaging"],
   metadataBase: new URL("https://sakshamparag.com"),
   openGraph: {
     title: "Saksham Parag Agarwal",
-    description: "Saksham Parag Agarwal, a 21-year-old product designer, intricately weaves inspiration from music and comics into his designs, reflecting a commitment to eco-consciousness and the right to repair in the design world.",
+    description: `Saksham Parag Agarwal, a ${getAge()}-year-old product designer, intricately weaves inspiration from music and comics into his designs, reflecting a commitment to eco-consciousness and the right to repair in the design world.`,
     type: "website",
   },
   twitter: {
     title: "Saksham Parag Agarwal",
-    description: "Saksham Parag Agarwal, a 21-year-old product designer, intricately weaves inspiration from music and comics into his designs, reflecting a commitment to eco-consciousness and the right to repair in the design world.",
+    description: `Saksham Parag Agarwal, a ${getAge()}-year-old product designer, intricately weaves inspiration from music and comics into his designs, reflecting a commitment to eco-consciousness and the right to repair in the design world.`,
   },
 };
 
